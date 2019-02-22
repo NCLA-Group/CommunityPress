7fe9a7baf020affd0a477625e9514b089bd8360c
Jimmy Business <jimmyb@neutroncreative.com>
2019-02-21 19:08:51 -0500

    🏗 Add index file & plugin details

---
+++ communitypress.php
@@ -0,0 +1,11 @@
+<?
+/**
+ * Plugin Name: CommunityPress
+ * Plugin URI: https://github.com/Neutron-Creative/CommunityPress
+ * Description: An open-source extension to WordPress, outlining best practices in a minimal community standard. With CommunityPress, you can implement functionality missing from the WordPress core in a safe, reliable manner.
+ * Version: 0.1.0
+ * Author: NCLA Group
+ * Author URI: https://neutroncreative.com
+ * License: MIT
+ */
+?>
